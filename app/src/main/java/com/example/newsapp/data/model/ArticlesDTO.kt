45fe003d06d51407b88e0bd7e232b09c8671eeb3
danilorlov12 <danil.orlov.3022013@gmail.com>
2022-09-05 19:49:09 +0300

    Implement data layer

---
+++ app/src/main/java/com/example/newsapp/data/model/ArticlesDTO.kt
@@ -0,0 +1,15 @@
+package com.example.newsapp.data.model
+
+import com.example.newsapplication.data.model.SourceDTO
+import com.google.gson.annotations.SerializedName
+
+data class ArticlesDTO(
+    @SerializedName("source") val sourceDto: SourceDTO,
+    @SerializedName("author") val author: String,
+    @SerializedName("title") val title: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("url") val url: String,
+    @SerializedName("urlToImage") val urlToImage: String,
+    @SerializedName("publishedAt") val publishedAt: String,
+    @SerializedName("content") val content: String
+)
---
+++ app/src/main/java/com/example/newsapp/data/model/ResultDTO.kt
@@ -0,0 +1,10 @@
+package com.example.newsapplication.data.model
+
+import com.example.newsapp.data.model.ArticlesDTO
+import com.google.gson.annotations.SerializedName
+
+data class ResultDTO(
+    @SerializedName("status") val status: String,
+    @SerializedName("totalResults") val totalResults: Int,
+    @SerializedName("articles") val articlesList: List<ArticlesDTO>
+)
